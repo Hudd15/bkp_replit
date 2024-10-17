7a07775cc0163d237446fcb54a618c4281f49daa
Hudd15 <hudsondiasdelima15@gmail.com>
2024-10-17 11:18:19 -0300

    Formcolor

---
+++ Formcolor/_JS_MOBILE/funcoes01.js
@@ -0,0 +1,21 @@
+// Atividades funções Javascript
+// 1) função soma(n1.n2)
+// 2) função soma(n1, n2) com checagem IF/ELSE
+// 3) função nomeAltura(nome, altura) comchecagem IF/ELSE
+// 4) função numeroParOuImpar(n1)
+
+//DECLARAÇÃO DE FUNÇÃO
+
+function Dadospessoais (nome,altura) {
+  console.log("Oi!" + nome + "tu tens" + altura + "metros de altura")
+
+} 
+Dadospessoais("Romeu", 1.90)
+
+
+
+function soma(n1, n2) {
+  const res = n1 + n2
+  console.log("resultado" + res)
+}
+//soma(10, 20)
